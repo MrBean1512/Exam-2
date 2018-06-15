81574d5cdc2c04ad663971c88f2ccd6728938acf
MrBean1512 <39568733+MrBean1512@users.noreply.github.com>
2018-06-15 02:14:06 -0700

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -1,4 +1,4 @@
-I affirm that all code given below was written solely by me, David Martin, and that any help I received adhered to the rules stated for this exam.
+//I affirm that all code given below was written solely by me, David Martin, and that any help I received adhered to the rules stated for this exam.
 #include "Runner.h"
 #include <iostream>
 #include <string>
